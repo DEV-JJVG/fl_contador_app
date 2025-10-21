2bb0fdabfa7e1e447029b32f868ccc67dbb6f689
DEV-JJVG <204841338+DEV-JJVG@users.noreply.github.com>
2025-10-21 10:56:37 +0200

    separamos el widget y declaramos las funciones para crear un código mucho más limpio

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int contador = 0;
+  void incrementar() {
+    setState(() => contador++);
+  }
+
+  void restablecer() {
+    setState(() => contador = 0);
+  }
+
+  void decrementar() {
+    setState(() => contador--);
+  }
+
   @override
   Widget build(BuildContext context) {
     const size30 = TextStyle(fontSize: 30.0);
@@ -28,37 +40,47 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              print("Estoy presionando el botón");
-              setState(() => contador++);
-              print(contador);
-            },
-          ),
-          SizedBox(width: 20),
-          FloatingActionButton(
-            child: const Text('Reset'),
-            onPressed: () {
-              print("Estoy presionando el botón");
-              setState(() => contador = 0);
-              print(contador);
-            },
-          ),
-          SizedBox(width: 20),
-          FloatingActionButton(
-            child: const Icon(Icons.remove),
-            onPressed: () {
-              print("Estoy presionando el botón");
-              setState(() => contador--);
-              print(contador);
-            },
-          ),
-        ],
+
+      floatingActionButton: CustomizedFloatingActionButton(
+        incrementarFn: incrementar,
+        restablecerFn: restablecer,
+        decrementarFn: decrementar,
       ),
+    );
+  }
+}
+
+class CustomizedFloatingActionButton extends StatelessWidget {
+  final Function incrementarFn;
+  final Function restablecerFn;
+  final Function decrementarFn;
+  const CustomizedFloatingActionButton({
+    super.key,
+    required this.incrementarFn,
+    required this.restablecerFn,
+    required this.decrementarFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => incrementarFn(),
+        ),
+        SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Text('Reset'),
+          onPressed: () => restablecerFn(),
+        ),
+        SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          onPressed: () => decrementarFn(),
+        ),
+      ],
     );
   }
 }
