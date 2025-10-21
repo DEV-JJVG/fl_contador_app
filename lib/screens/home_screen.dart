477f533dc8307ba7b21906f2ca7a5ff2e68ed5fb
DEV-JJVG <204841338+DEV-JJVG@users.noreply.github.com>
2025-10-21 10:38:12 +0200

    Agrego dos botones, uno para resetear el contador y otro para decrementar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,14 +27,35 @@ class _HomeScreenState extends State<HomeScreen> {
           ]),
         ),
       ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FloatingActionButton(
             child: const Icon(Icons.add),
             onPressed: () {
               print("Estoy presionando el botón");
               contador++;
+              setState(() {});
+              print(contador);
+            },
+          ),
+          SizedBox(width: 20),
+          FloatingActionButton(
+            child: const Text('Reset'),
+            onPressed: () {
+              print("Estoy presionando el botón");
+              contador = 0;
+              setState(() {});
+              print(contador);
+            },
+          ),
+          SizedBox(width: 20),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () {
+              print("Estoy presionando el botón");
+              contador--;
               setState(() {});
               print(contador);
             },
