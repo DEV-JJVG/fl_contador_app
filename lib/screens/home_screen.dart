0131378a027aa6338c1ee39048e324acbd63feab
DEV-JJVG <204841338+DEV-JJVG@users.noreply.github.com>
2025-10-21 10:12:34 +0200

    Creamos un contador con un botón para que cada vez que pulsemos el contador el número se incremente

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int contador = 0;
+  @override
   Widget build(BuildContext context) {
+    const size30 = TextStyle(fontSize: 30.0);
+
     return Scaffold(
       backgroundColor: Colors.cyanAccent,
-      appBar: AppBar(title: Text("HomeScreen"), elevation: 10.0),
+      appBar: AppBar(title: Text("Contador"), elevation: 10.0),
 
       body: Center(
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: ([
-            Text("Hola Mundo desde mi nuevo HomeScreen"),
-            SizedBox(height: 100),
-            Text("Bocata lomo ya"),
+            const Text("Numero de clicks", style: size30),
+            const SizedBox(height: 100),
+            Text('$contador', style: size30),
           ]),
         ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              print("Estoy presionando el botón");
+              contador++;
+              setState(() {});
+              print(contador);
+            },
+          ),
+        ],
       ),
     );
   }
